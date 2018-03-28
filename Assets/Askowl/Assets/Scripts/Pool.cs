96b70b240296a8592898557b353956aeb33eda96
paulmarrington <paul@marrington.net>
2018-03-28 19:34:54 +1000

    Singleton and Pool Examples

--- Assets/Askowl/Assets/Scripts/Pool.cs
+++ Assets/Askowl/Assets/Scripts/Pool.cs
@@ -1,20 +1,29 @@
 ﻿namespace Askowl {
   using System.Collections.Generic;
+  using JetBrains.Annotations;
   using UnityEngine;
 
-  public class Pool<T> : MonoBehaviour where T : Component {
+  public class Pool<T> : Singleton<Pool<T>> where T : Component {
     [SerializeField] private T original;
 
-    public static Pool<T> Instance { get; private set; }
+    private readonly Queue<T> clones = new Queue<T>();
 
-    private static readonly Queue<T> clones = new Queue<T>();
-
-    private void Awake() { Instance = this; }
-
-    public T Get() {
+    public T Get([CanBeNull] Transform parent = default(Transform),
+                 bool                  enable = true) {
       if (clones.Count == 0) DeepenPool();
       T clone = clones.Dequeue();
-      clone.gameObject.SetActive(true);
+      clone.gameObject.SetActive(enable);
+      clone.transform.SetParent(parent);
+      return clone;
+    }
+
+    public T Get(Vector3               position = default(Vector3),
+                 Quaternion            rotation = default(Quaternion),
+                 [CanBeNull] Transform parent   = default(Transform),
+                 bool                  enable   = true) {
+      T clone = Get(parent, enable);
+      clone.transform.position = position;
+      clone.transform.rotation = rotation;
       return clone;
     }
 
