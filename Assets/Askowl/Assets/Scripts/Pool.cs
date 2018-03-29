3e4bc209a930307baed0b6c76b07782c7a2c0d5d
paulmarrington <paul@marrington.net>
2018-03-29 13:07:52 +1000

    finished Pool - ready to test

--- Assets/Askowl/Assets/Scripts/Pool.cs
+++ Assets/Askowl/Assets/Scripts/Pool.cs
@@ -1,27 +1,53 @@
-﻿using UnityEngine;
-
-namespace Askowl {
+﻿namespace Askowl {
   using System;
   using System.Collections.Generic;
   using JetBrains.Annotations;
   using UnityEngine;
   using Object = UnityEngine.Object;
 
-  public class Pool<T> where T : Component {
-    public int    Depth { get; private set; }
-    public string Name  { get; private set; }
+  public interface IPool {
+    void Prepare(string name, Component master);
+  }
 
-    private Queue<T> clones = new Queue<T>();
-    private T        original;
+  public sealed class PooledMonobehaviour : MonoBehaviour {
+    [SerializeField] public Component Original;
+  }
+
+  public class Pool<T> : IPool where T : Component {
+    public int Depth { get; private set; }
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    public string Name { [UsedImplicitly] get; private set; }
+
+    private readonly Queue<T> clones = new Queue<T>();
+    private          T        original;
 
     private static Pool<T> instance;
+
+    public static void CreatePool(T ofComponent) {
+      if (instance != null) return;
+
+      Type                poolType  = ofComponent.GetType();
+      GameObject          singleton = new GameObject();
+      PooledMonobehaviour component = singleton.AddComponent<PooledMonobehaviour>();
+
+      component.Original = ofComponent;
+      singleton.name     = component.name = "Pooled " + poolType.Name;
+
+      Type genericType = typeof(Pool<>).MakeGenericType(poolType);
+      instance = Activator.CreateInstance(type: genericType) as Pool<T>;
+
+      if (instance != null) instance.Prepare(name: ofComponent.name, master: ofComponent);
+
+      Object.DontDestroyOnLoad(target: singleton);
+    }
 
     [NotNull]
     public static Pool<T> Instance { get { return instance ?? (instance = new Pool<T>()); } }
 
-    protected static void Prepare([CanBeNull] string name = null, T master = null) {
+    public void Prepare([CanBeNull] string name, Component master) {
       Instance.Name     = string.IsNullOrEmpty(value: name) ? typeof(T).Name : name;
-      Instance.original = master;
+      Instance.original = master as T;
     }
 
     [NotNull]
@@ -56,16 +82,4 @@ namespace Askowl {
       return clone;
     }
   }
-
-public class PooledMonobehaviour : MonoBehaviour {
-  [SerializeField] private Component original;
-
-  private void Awake() {
-
-    typeof(Pool<>).MakeGenericType(original.GetType());
-
-    Type d1 = typeof(Pool<>);
-    Type[] typeArgs = {original.GetType()};
-  }
-}
 }
