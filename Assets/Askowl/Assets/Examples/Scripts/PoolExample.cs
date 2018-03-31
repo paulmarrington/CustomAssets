7aeacf51cf140be456a6be76658f910da8b4f6a0
paulmarrington <paul@marrington.net>
2018-03-31 22:01:58 +1000

    Pool and associated test done (doco to go)

--- Assets/Askowl/Assets/Examples/Scripts/PoolExample.cs
+++ Assets/Askowl/Assets/Examples/Scripts/PoolExample.cs
@@ -10,33 +10,33 @@ public sealed class PoolExample : MonoBehaviour {
   IEnumerator PoolTest() {
     yield return new WaitForSeconds(0.5f);
 
-    Assert.IsNotNull(Pools.PoolFor("PoolSamplePrefab"));
-    Assert.IsNotNull(Pools.PoolFor("PoolPrefabScriptSample"));
+    Assert.IsNotNull(Pool.PoolFor("PoolSamplePrefab"));
+    Assert.IsNotNull(Pool.PoolFor("PoolPrefabScriptSample"));
 
     PoolPrefabScriptSample[] prefab1 = new PoolPrefabScriptSample[21];
-    Assert.AreEqual(0, Pools.PoolFor("PoolSamplePrefab").Count);
+    Assert.AreEqual(0, Pool.PoolFor("PoolSamplePrefab").Count);
 
     for (int i = 0; i < 21; i++) {
-      prefab1[i] = Pools.Get<PoolPrefabScriptSample>("PoolSamplePrefab");
+      prefab1[i] = Pool.Acquire<PoolPrefabScriptSample>("PoolSamplePrefab");
 
       yield return new WaitForSeconds(frequency);
 
       prefab1[i].MaxCount = i;
     }
 
-    Assert.AreEqual(expected: 0, actual: Pools.PoolFor("PoolSamplePrefab").Count);
+    Assert.AreEqual(expected: 0, actual: Pool.PoolFor("PoolSamplePrefab").Count);
 
     PoolPrefabScriptSample[] prefab2 = new PoolPrefabScriptSample[21];
 
     for (int i = 0; i < 21; i++) {
-      prefab2[i] = Pools.Get<PoolPrefabScriptSample>();
+      prefab2[i] = Pool.Acquire<PoolPrefabScriptSample>();
       yield return new WaitForSeconds(frequency);
 
       prefab2[i].MaxCount = 100 + i;
     }
 
-    Assert.AreEqual(expected: 0, actual: Pools.PoolFor("PoolSamplePrefab").Count);
-    Assert.AreEqual(expected: 0, actual: Pools.PoolFor("PoolPrefabScriptSample").Count);
+    Assert.AreEqual(expected: 0, actual: Pool.PoolFor("PoolSamplePrefab").Count);
+    Assert.AreEqual(expected: 0, actual: Pool.PoolFor("PoolPrefabScriptSample").Count);
 
     for (int i = 0; i < 7; i++) {
       prefab1[i].gameObject.SetActive(false);
@@ -48,25 +48,25 @@ public sealed class PoolExample : MonoBehaviour {
       yield return new WaitForSeconds(frequency);
     }
 
-    Assert.AreEqual(expected: 7, actual: Pools.PoolFor("PoolSamplePrefab").Count);
-    Assert.AreEqual(expected: 8, actual: Pools.PoolFor("PoolPrefabScriptSample").Count);
+    Assert.AreEqual(expected: 7, actual: Pool.PoolFor("PoolSamplePrefab").Count);
+    Assert.AreEqual(expected: 9, actual: Pool.PoolFor("PoolPrefabScriptSample").Count);
 
-    Assert.IsNotNull(Pools.PoolFor("Scene GameObject"));
+    Assert.IsNotNull(Pool.PoolFor("Scene GameObject"));
 
     GameObject[] scenes = new GameObject[21];
 
     for (int i = 0; i < 21; i++) {
-      scenes[i] = Pools.Get("Scene GameObject");
+      scenes[i] = Pool.Acquire("Scene GameObject");
       yield return new WaitForSeconds(frequency);
     }
 
-    Assert.AreEqual(expected: 0, actual: Pools.PoolFor("Scene GameObject").Count);
+    Assert.AreEqual(expected: 0, actual: Pool.PoolFor("Scene GameObject").Count);
 
     for (int i = 15; i < 19; i++) {
       scenes[i].gameObject.SetActive(false);
       yield return new WaitForSeconds(frequency);
     }
 
-    Assert.AreEqual(expected: 4, actual: Pools.PoolFor("Scene GameObject").Count);
+    Assert.AreEqual(expected: 4, actual: Pool.PoolFor("Scene GameObject").Count);
   }
 }
---
+++ Assets/Askowl/Assets/Scripts/Pool.cs
@@ -0,0 +1,139 @@
+﻿namespace Askowl {
+  using System.Collections.Generic;
+  using JetBrains.Annotations;
+  using UnityEngine;
+
+  public sealed class Pool : MonoBehaviour {
+    private void OnEnable() {
+      DontDestroyOnLoad(target: gameObject);
+
+      for (int i = 0; i < transform.childCount; ++i) {
+        CreatePool(transform.GetChild(i).gameObject);
+      }
+    }
+
+    private void CreatePool([NotNull] GameObject master) {
+      var poolName = string.Format("{0} Pool", master.name);
+
+      if (!Queues.ContainsKey(poolName)) {
+        PoolMonitor poolMonitor = master.AddComponent<PoolMonitor>();
+        poolMonitor.MasterName = master.name;
+        Queues[poolName]       = new PoolQueue {Master = master};
+      } else {
+        Debug.LogFormat("Duplicate Pools for {0}", master.name);
+      }
+    }
+
+    [CanBeNull]
+    public static T Acquire<T>([CanBeNull] string    name          = null,
+                               Vector3               position      = default(Vector3),
+                               Quaternion            rotation      = default(Quaternion),
+                               [CanBeNull] Transform parent        = default(Transform),
+                               bool                  enable        = true,
+                               bool                  poolOnDisable = true)
+      where T : Component {
+      name = name ?? typeof(T).Name;
+      GameObject clone = Acquire(name, position, rotation, parent, enable, poolOnDisable);
+      return (clone == null) ? null : clone.GetComponent<T>();
+    }
+
+    [CanBeNull]
+    public static GameObject Acquire(string                name,
+                                     Vector3               position      = default(Vector3),
+                                     Quaternion            rotation      = default(Quaternion),
+                                     [CanBeNull] Transform parent        = null,
+                                     bool                  enable        = true,
+                                     bool                  poolOnDisable = true) {
+      PoolQueue pool = PoolFor(name);
+      if (pool == null) return null;
+
+      GameObject  clone;
+      PoolMonitor poolMonitor;
+
+      do {
+        clone       = pool.Fetch();
+        poolMonitor = clone.gameObject.GetComponent<PoolMonitor>();
+      } while (!poolMonitor.OkToPool);
+
+      clone.transform.SetParent(parent ?? pool.Master.transform.parent);
+      clone.transform.position  = position;
+      clone.transform.rotation  = rotation;
+      poolMonitor.PoolOnDisable = poolOnDisable;
+      clone.gameObject.SetActive(enable);
+      poolMonitor.InPool   = false;
+      poolMonitor.OkToPool = true;
+      return clone;
+    }
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    public static void Return([NotNull] GameObject gameObject) {
+      PoolMonitor poolMonitor = gameObject.GetComponent<PoolMonitor>();
+      PoolQueue   pool        = PoolFor(poolMonitor.MasterName);
+
+      if (pool != null) {
+        poolMonitor.InPool = poolMonitor.OkToPool = true;
+        poolMonitor.gameObject.SetActive(false);
+        pool.Enqueue(gameObject);
+      } else {
+        Debug.LogErrorFormat("**** Error: {0} was not created in a pool", gameObject.name);
+      }
+
+      gameObject.SetActive(false);
+    }
+
+    [CanBeNull]
+    public static PoolQueue PoolFor(string name) {
+      string poolName = string.Format("{0} Pool", name);
+      return Queues.ContainsKey(poolName) ? Queues[poolName] : null;
+    }
+
+    [CanBeNull, UsedImplicitly]
+    private static PoolQueue PoolFor([NotNull] GameObject gameObject) {
+      return PoolFor(gameObject.GetComponent<PoolMonitor>().MasterName);
+    }
+
+    private sealed class PoolDict : Dictionary<string, PoolQueue> { }
+
+    private static readonly PoolDict Queues = new PoolDict();
+
+    private sealed class PoolMonitor : MonoBehaviour {
+      public bool   PoolOnDisable;
+      public bool   InPool, OkToPool = true;
+      public string MasterName;
+
+//      private void Awake() { DontDestroyOnLoad(gameObject); }
+
+      private void OnDisable() {
+        OkToPool = true;
+        if (PoolOnDisable && !InPool) Return(gameObject);
+      }
+
+      private void OnEnable() {
+        if (InPool || !PoolOnDisable) return;
+
+        OkToPool = false;
+        Debug.LogErrorFormat("Pooled '{0}' should not be reused", gameObject.name);
+      }
+    }
+
+    public sealed class PoolQueue : Queue<GameObject> {
+      public GameObject Master;
+
+      private int count = 0;
+
+      [NotNull]
+      public GameObject Fetch() {
+        GameObject clone;
+
+        if (Count > 0) {
+          clone = Dequeue();
+        } else {
+          clone      = Instantiate(Master);
+          clone.name = string.Format("{0} (clone {1})", Master.name, ++count);
+        }
+
+        return clone;
+      }
+    }
+  }
+}
