9f11c398410de3aad924156655e8bb5a7b275b2a
paulmarrington <paul@marrington.net>
2018-06-24 21:37:25 +1000

    24/06/2018

--- Assets/Askowl/CustomAssets/Scripts/Mutable/String.cs
+++ Assets/Askowl/CustomAssets/Scripts/Mutable/String.cs
@@ -8,8 +8,9 @@ namespace CustomAsset.Mutable {
   /// CustomAsset that contains a string. Events are triggered every time the string changes
   /// </summary>
   /// <remarks><a href="http://customassets.marrington.net#primitive-custom-assets">More...</a></remarks>
-  [CreateAssetMenu(menuName = "Custom Assets/Mutable/String")]
+  [CreateAssetMenu(menuName = "Custom Assets/Mutable/String"), ValueName("Text")]
   public sealed class String : OfType<string> {
+    public        string Text                  { get { return Value; } set { Value = value; } }
     public static String Instance(string name) { return Instance<String>(name); }
   }
 }
