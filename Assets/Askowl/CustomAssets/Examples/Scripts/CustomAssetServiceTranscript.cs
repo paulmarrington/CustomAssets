62e528270e39e95abf31097007fa1ded67111221
paulmarrington <paul@marrington.net>
2019-01-19 18:25:08 +1000

    19/01/2019

---
+++ Assets/Askowl/CustomAssets/Examples/Scripts/CustomAssetServiceTranscript.cs
@@ -0,0 +1,4 @@
+﻿//- Copyright 2018 (C) paul@marrington.net http://www.askowl.net/unity-packages
+//- Creating a new service is easy. Just select the Assets or Project context menu 'Create // Custom Assets // Services // Create Service'
+//- You will be asked to give a name to your service. Make the name descriptive. Your service will be created in a directory of the same name. Rename or move it as you wish.
+//- If the service directory isn't under a scripts directory you will need to add an assembly definition (Assets or Project context menu 'Create // Assembly Definition'
