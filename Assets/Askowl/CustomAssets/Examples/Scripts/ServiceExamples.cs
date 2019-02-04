04fc69479943c31c4d37a1a9a01f1581b3bb935c
paulmarrington <paul@marrington.net>
2019-02-04 20:33:21 +1000

    bdd

--- Assets/Askowl/CustomAssets/Examples/Scripts/ServiceExamples.cs
+++ Assets/Askowl/CustomAssets/Examples/Scripts/ServiceExamples.cs
@@ -11,13 +11,13 @@ using UnityEngine.TestTools;
 
 namespace Askowl.Examples {
   public class ServiceExamples : PlayModeTests {
-    [UnityTest] public IEnumerator Basic() {
+    [UnityTest] public IEnumerator TopDownSuccess() {
       var mockState = Manager.Load<String>("MockState.asset");
+      mockState.Text = "Success";
 
-      var manager = Manager.Load<ServiceExampleServicesManager>("ServiceExampleServicesManager.asset");
-      var mathServer  = (ServiceExampleServiceAdapter) manager.Instance;
+      var manager    = Manager.Load<ServiceExampleServicesManager>("ServiceExampleServicesManager.asset");
+      var mathServer = (ServiceExampleServiceAdapter) manager.Instance;
       var addService = mathServer.Service<ServiceExampleServiceAdapter.AddDto>();
-      if (addService.Error) Assert.Fail(addService.ErrorMessage);
 
       var dto = addService.Dto;
       dto.request = new ServiceExampleServiceAdapter.AddDto.Request {firstValue = 21, secondValue = 22};
@@ -25,9 +25,31 @@ namespace Askowl.Examples {
 
       yield return Fiber.Start
                         .WaitFor(mathServer.Call(addService))
+                        .Do(_ => Assert.IsFalse(addService.Error, addService.ErrorMessage))
                         .Do(_ => Assert.AreEqual(43, dto.result))
                         .AsCoroutine();
     }
+
+    [UnityTest] public IEnumerator ServiceError() {
+      var mockState = Manager.Load<String>("MockState.asset");
+      mockState.Text = "ServiceFailure";
+
+      var manager    = Manager.Load<ServiceExampleServicesManager>("ServiceExampleServicesManager.asset");
+      var mathServer = (ServiceExampleServiceAdapter) manager.Instance;
+      var addService = mathServer.Service<ServiceExampleServiceAdapter.AddDto>();
+
+      var dto = addService.Dto;
+      dto.request = new ServiceExampleServiceAdapter.AddDto.Request {firstValue = 21, secondValue = 22};
+      dto.result  = 0; // don't need to set this except to ensure test validity
+
+      yield return Fiber.Start
+                        .WaitFor(mathServer.Call(addService))
+                        .Do(_ => Assert.IsFalse(addService.Error, addService.ErrorMessage))
+                        .Do(_ => Assert.AreEqual(43, dto.result))
+                        .AsCoroutine();
+    }
+
+    [UnityTest] public IEnumerator ServiceFallback() { yield return null; }
   }
 }
 #endif
--- Assets/Askowl/CustomAssets/Examples/ServiceExample/ServiceExampleServiceForMock.cs
+++ Assets/Askowl/CustomAssets/Examples/ServiceExample/ServiceExampleServiceForMock.cs
@@ -9,7 +9,8 @@ namespace CustomAsset.Services {
   [CreateAssetMenu(
     menuName = "Custom Assets/Services/ServiceExample/ServiceForMock", fileName = "ServiceExampleServiceForMock")]
   public class ServiceExampleServiceForMock : ServiceExampleServiceAdapter {
-    [SerializeField] private String mockState;
+    [SerializeField] private String mockState = default;
+
     /// <a href="">Prepare the mock service for operations</a> //#TBD#//
     protected override void Prepare() => base.Prepare();
 
@@ -17,9 +18,18 @@ namespace CustomAsset.Services {
     protected override void LogOnResponse(Emitter emitter) => base.LogOnResponse(emitter);
 
     public override Emitter Call(Service<AddDto> service) {
-      service.Dto.result = service.Dto.request.firstValue + service.Dto.request.secondValue;
-      Fiber.Start.WaitFor(seconds: 0.1f).Fire(service.Emitter);
-      return service.Emitter;
+      switch (mockState.Text) {
+        case "Success":
+          service.Dto.result = service.Dto.request.firstValue + service.Dto.request.secondValue;
+          Fiber.Start.WaitFor(seconds: 0.1f).Fire(service.Emitter);
+          return service.Emitter;
+        case "ServiceFailure":
+          service.ErrorMessage = "Service Failure";
+          return null;
+        default:
+          service.ErrorMessage = $"Unknown mock state {mockState.Text}";
+          return null;
+      }
     }
     /// <inheritdoc />
     public override bool IsExternalServiceAvailable() => true;
