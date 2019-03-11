9ad248e4f5e3a2d218ac7df47c7ae6652266a928
paulmarrington <paul@marrington.net>
2019-03-11 18:12:14 +1000

    11/03/2019

--- Assets/Askowl/CustomAssets/Editor/AssetWizard.cs
+++ Assets/Askowl/CustomAssets/Editor/AssetWizard.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Text.RegularExpressions;
 using CustomAsset;
 using UnityEditor;
@@ -15,23 +16,23 @@ using String = CustomAsset.Constant.String;
 namespace Askowl {
   /// <a href=""></a> //#TBD#//
   public abstract class AssetWizard : ScriptableObject {
-    private static string      assetType, destination, destinationName, destinationRelative;
-    private static AssetWizard wizard;
-    // [MenuItem("Assets/Create/NAME")]
     /// <a href=""></a> //#TBD#//
-    public void CreateAssets(string newAssetType, params string[] templateSubstitutions) {
+    protected static string assetType, destination, destinationName;
+    /// <a href=""></a> //#TBD#//
+    protected static AssetWizard wizard;
+    /// <a href=""></a> //#TBD#//
+    protected void CreateAssets(string newAssetType) {
       Selection.activeObject = null; // in case we had a creation form inspector up
       if (wizard != null) throw new Exception($"Please wait for asset builder {assetType} to complete");
       assetType = newAssetType;
       wizard    = this;
       SetDestination();
-      bool hasSource = ProcessSource(@"cs|txt", templateSubstitutions);
-      destinationRelative = destination.Substring(destination.IndexOf("/Assets/", StringComparison.Ordinal) + 1);
+      bool hasSource = ProcessAllFiles(@"cs|txt");
       AssetDatabase.ImportAsset(
-        Path.GetDirectoryName(destinationRelative)
+        Path.GetDirectoryName(destination)
       , ImportAssetOptions.ForceSynchronousImport | ImportAssetOptions.ImportRecursive);
       if (hasSource) {
-        Debug.Log($"Scripts for `{destinationRelative}` waiting on rebuild for basic assets...");
+        Debug.Log($"Scripts for `{destination}` waiting on rebuild for basic assets...");
         // Will continue in `OnScriptsReloaded` if there is source to recompile
       } else {
         Phase2();
@@ -46,31 +47,45 @@ namespace Askowl {
       wizard.OnScriptReload();
       wizard.SaveAssetDictionary();
       wizard = null;
+      Debug.Log("...All Done");
     }
 
-    private static bool ProcessSource(string textAssetTypes, string[] templateSubstitutions) {
-      if (templateSubstitutions.Length == 0) return false;
+    private static bool ProcessAllFiles(string textAssetTypes) {
+      Regex    textAssetTypesRegex                        = new Regex($@"\.({textAssetTypes})$");
+      string[] sources                                    = AssetDatabase.FindAssets("", new[] {TemplatePath()});
+      for (int i = 0; i < sources.Length; i++) sources[i] = AssetDatabase.GUIDToAssetPath(sources[i]);
+      return ProcessFiles(textAssetTypes, sources);
+    }
+
+    /// <a href=""></a> //#TBD#//
+    protected static bool ProcessFiles(string textAssetTypes, params string[] files) {
       bool     hasSource           = false;
-      Regex    textAssetTypesRegex = new Regex($"\\.({textAssetTypes})$");
+      Regex    textAssetTypesRegex = new Regex($@"\.({textAssetTypes})$");
       string[] sources             = AssetDatabase.FindAssets("", new[] {TemplatePath()});
       using (var template = Template.Instance) {
         template.Substitute("Template", destinationName);
-        for (int i = 0; i < sources.Length; i++) {
-          var sourcePath = AssetDatabase.GUIDToAssetPath(sources[i]);
-          var fileName   = Path.GetFileName(sourcePath);
-          if (File.Exists(sourcePath)) {
-            if (textAssetTypesRegex.IsMatch(sourcePath)) {
-              var text                                  = template.Process(File.ReadAllText(sourcePath));
-              if (sourcePath.EndsWith(".cs")) hasSource = true;
-              File.WriteAllText($"{destination}/{assetType}{fileName}", text);
+        for (int i = 0; i < files.Length; i++) {
+          var fileName = Path.GetFileName(files[i]) ?? "";
+          if (File.Exists(files[i])) {
+            if (textAssetTypesRegex.IsMatch(files[i])) {
+              if (files[i].EndsWith(".cs")) hasSource = true;
+              var text                                = wizard.FillTemplate(template, File.ReadAllText(files[i]));
+              if (fileName.StartsWith(destinationName)) {
+                File.WriteAllText($"{destination}/{fileName}", text);
+              } else {
+                File.WriteAllText($"{destination}/{destinationName}{fileName}", text);
+              }
             } else {
-              File.Copy(sourcePath, $"{destination}/{fileName}");
+              File.Copy(files[i], $"{destination}/{fileName}");
             }
           }
         }
       }
       return hasSource;
     }
+
+    /// <a href=""></a> //#TBD#//
+    protected virtual string FillTemplate(Template template, string text) => template.From(text).Result();
 
     private static string TemplatePath() {
       var paths = AssetDatabase.FindAssets($"{assetType}TemplatePath");
@@ -85,18 +100,6 @@ namespace Askowl {
         return Directory.Exists(path) ? path : null;
       }
       return exists("Editor/Template") ?? exists("Template") ?? exists("scripts/Template") ?? "";
-    }
-
-    private static string GetSelectedPathOrFallback() {
-      string path = "Assets";
-      foreach (Object obj in Selection.GetFiltered(typeof(Object), SelectionMode.Assets)) {
-        path = AssetDatabase.GetAssetPath(obj);
-        if (!string.IsNullOrEmpty(path) && File.Exists(path)) {
-          path = Path.GetDirectoryName(path);
-          break;
-        }
-      }
-      return path;
     }
 
     private readonly Dictionary<string, SerializedObject> assets = new Dictionary<string, SerializedObject>();
@@ -188,7 +191,7 @@ namespace Askowl {
       var manager = GetCustomAssetManager();
       foreach (var entry in assets) {
         if (entry.Value.targetObject.GetType().IsSubclassOf(typeof(ScriptableObject))) {
-          string assetName = $"{destinationRelative}/{destinationName} {assetType} {entry.Key}.asset";
+          string assetName = $"{destination}/{destinationName} {assetType} {entry.Key}.asset";
           AssetDatabase.CreateAsset(entry.Value.targetObject, assetName);
           if (entry.Key.EndsWith("Manager")) InsertIntoArrayField(manager, "managers", entry.Value.targetObject);
         }
@@ -205,7 +208,7 @@ namespace Askowl {
       if (gameObject == default) {
         var prefab = Resources.Load(prefabName);
         if (prefab == null) throw new Exception($"No prefab named {prefabName}");
-        gameObject      = (GameObject) Object.Instantiate(prefab, Vector3.zero, Quaternion.identity);
+        gameObject      = (GameObject) Instantiate(prefab, Vector3.zero, Quaternion.identity);
         gameObject.name = prefabName;
       }
       var component = gameObject.GetComponentInChildren(monoBehaviour);
@@ -217,10 +220,11 @@ namespace Askowl {
 
     /// <a href=""></a> //#TBD#//
     protected virtual string GetDestinationPath() => EditorUtility.SaveFilePanel(
-      $"Location for your new {assetType}", GetSelectedPathOrFallback(), "", "");
+      $"Location for your new {assetType}", GetSelectedPathInProjectView(), "", "");
 
     private void SetDestination() {
       destination     = GetDestinationPath();
+      destination     = destination.Substring(destination.IndexOf("/Assets/", StringComparison.Ordinal) + 1);
       destinationName = Path.GetFileNameWithoutExtension(destination);
       if (string.IsNullOrWhiteSpace(destination)) Fatal("Enter name for the new asset");
       if (Directory.Exists(destination)) {
@@ -231,5 +235,56 @@ namespace Askowl {
 
     /// <a href=""></a> //#TBD#//
     protected void Fatal(string msg) => throw new Exception(msg);
+
+    /// <a href=""></a> //#TBD#//
+    protected string[] ToDefinitions(string text) => csRegex.Split(text);
+
+    /// <a href=""></a> //#TBD#//
+    protected string ToTuple(string fields) {
+      var pairs = ToDefinitions(fields);
+      if (pairs.Length == 1) return pairs[0];
+      builder.Clear().Append("(");
+      for (int i = 0; i < fields.Length; i += 2) builder.Append($"{fields[i]} {fields[i + 1]}");
+      return builder.Append($")").ToString();
+    }
+    private readonly        StringBuilder builder = new StringBuilder();
+    private static readonly Regex         csRegex = new Regex(@"\s*;\s*|\s*,\s*|\s+", RegexOptions.Singleline);
+
+    /// <a href=""></a> //#TBD#//
+    protected static string GetSelectedPathInProjectView() {
+      string path = "Assets";
+      EditorUtility.FocusProjectWindow();
+      Debug.Log($"*** GetSelectedPathInProjectView '{Selection.activeObject}'"); //#DM#//
+      foreach (Object obj in Selection.GetFiltered(typeof(Object), SelectionMode.Assets)) {
+        path = AssetDatabase.GetAssetPath(obj);
+        if (!string.IsNullOrEmpty(path) && File.Exists(path)) {
+          path = Path.GetDirectoryName(path);
+          break;
+        }
+      }
+      return path;
+    }
+
+    [MenuItem("TEST/TEST")] static void SelectPath() {
+      Debug.Log($"*** SelectPath '{GetSelectedPathInProjectView()}'"); //#DM#//
+    }
+
+    /// <a href=""></a> //#TBD#//
+    [CustomEditor(typeof(AssetWizard), true)] public class AssetWizardEditor : Editor {
+      public override void OnInspectorGUI() {
+        if (GUILayout.Button("Clear")) {
+          ((AssetWizard) target).Clear();
+          GUI.FocusControl(null);
+        }
+        serializedObject.Update();
+        DrawDefaultInspector();
+        if (GUILayout.Button("Create")) ((AssetWizard) target).Create();
+        serializedObject.ApplyModifiedProperties();
+      }
+    }
+    /// <a href=""></a> //#TBD#//
+    protected virtual void Create() => throw new NotImplementedException();
+    /// <a href=""></a> //#TBD#//
+    protected virtual void Clear() => throw new NotImplementedException();
   }
 }
